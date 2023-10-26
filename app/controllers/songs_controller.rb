b557258140002df481fd403e9919a929b9b17044
Jack <jconnolly444@gmail.com>
2023-10-26 14:03:54 -0500

    edited and got restful actions working

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,15 +8,15 @@ class SongsController < ApplicationController
     render :show
   end
   def create
-    @song = Song.create(title: params["title"], album: params["album"],artist: params["artist"], year: params [:year])
+    @song = Song.create(title: params["title"], album: params["album"],artist: params["artist"], year: params[:year])
     render :show
   end
   def update
     @song = Song.find_by(id: params["id"])
     @song.update(
-      title: params["title"] || @song.name,
+      title: params["title"] || @song.title,
       album: params["album"] || @song.album,
-      artist: params["artist"] || @song.artist
+      artist: params["artist"] || @song.artist,
       year: params[:year] || @song.year)
       render :show
   end
