664338b782354311e02dc296d3900e142ae4ae2a
Jack <jconnolly444@gmail.com>
2023-10-26 13:20:14 -0500

    added restful actions and more

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Song.create(title: "Birthday Boy", album: "GodWeenSatan", artist: "Ween", year: 1990)
+
+Song.create(title: "My Name is Mud", album: "Pork Soda", artist: "Primus", year: 1993)
+
+Song.create(title: "Left of the Dial", album: "Tim", artist: "The Replacements", year: 1985)
+
+Song.create(title: "Vamos", album: "Surfer Rosa", artist: "Pixies", year: 1988)
+
+Song.create(title: "History Lesson, Pt. 2", album: "Double Nickels On The Dime", artist: "Minutemen", year: 1984)
+
